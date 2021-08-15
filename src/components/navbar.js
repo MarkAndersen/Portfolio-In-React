dbc437ec1d917a9476da72ee6f71135037bb5724
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-14 21:30:28 -0400

    playing with stylings, beginning construction of the page

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+function NavBar() {
+    return (
+        <nav style={{ color: "red"}}>
+            <p>Hello</p>
+
+        </nav>
+    );
+  }
+  
+  export default NavBar;
+// export default navbar;
