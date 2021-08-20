29642fe98d5fd42d6b1fb7b55ebf13f332e63b05
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-19 20:41:23 -0400

    added shell components to being react construction, imported bootstrap with NPM and react-bootstrap, will chose later down the line.

---
+++ src/components/Container.js
@@ -0,0 +1,32 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import NavBar from "./Navbar";
+import Project from "./Project";
+import React, { useState } from 'react';
+
+export default function Container() {
+    const [currentPage, setCurrentPage] = useState('Home');
+  
+    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+    const renderPage = () => {
+      if (currentPage === 'Home') {
+        return <Home />;
+      }
+      if (currentPage === 'Project') {
+        return <Project />;
+      }
+      return <Contact />;
+    };
+  
+    const handlePageChange = (page) => setCurrentPage(page);
+  
+    return (
+      <div>
+        {/* We are passing the currentPage from state and the function to update it */}
+        <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+        {/* Here we are calling the renderPage method which will return a component  */}
+        {renderPage()}
+      </div>
+    );
+  }
+  
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,51 @@
 import React from "react";
+// import Nav from "react-bootstrap/Nav"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function NavBar() {
     return (
-        <nav style={{ color: "red"}}>
-            <p>Hello</p>
-
-        </nav>
+    //     <Nav
+    //     activeKey="/home"
+    //     onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    //   >
+    //     <Nav.Item>
+    //       <Nav.Link href="/home">Active</Nav.Link>
+    //     </Nav.Item>
+    //     <Nav.Item>
+    //       <Nav.Link eventKey="link-1">Link</Nav.Link>
+    //     </Nav.Item>
+    //     <Nav.Item>
+    //       <Nav.Link eventKey="link-2">Link</Nav.Link>
+    //     </Nav.Item>
+    //     <Nav.Item>
+    //       <Nav.Link eventKey="disabled" disabled>
+    //         Disabled
+    //       </Nav.Link>
+    //     </Nav.Item>
+    //   </Nav>
+    // );
+    <nav class="navbar navbar-expand-md navbar-light pb-4"  style="background-color: #a6e681;">
+        
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #a6e681;">
+        <span class="navbar-toggler-icon" style="background-color: #a6e681;"></span>
+    </button>
+    
+    <div class="collapse navbar-collapse" id="navbarSupportedContent"  style="background-color: #a6e681;">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <button class="nav-link" href="#bio">About Me <span class="sr-only">(current)</span></button>
+            </li>
+            <li class="nav-item">
+                <button class="nav-link" href="#work">My Work</button>
+            </li>
+            <li class="nav-item">
+                <button class="nav-link" href="#contact">Contact Me</button>
+            </li>
+        </ul>
+        <button class="navbar-brand justify-content-center" href="#">Mark Andersen</button>
+    </div>
+</nav>
     );
   }
   
