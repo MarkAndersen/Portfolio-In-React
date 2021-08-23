7be1ec26ed4387354402e33229274dd4ca717c25
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-23 11:17:32 -0400

    switched to react-bootstraps form fields, handleformsubmit partially broken now

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,76 +1,140 @@
 import React, { useState } from "react";
 import { validateEmail } from "../../utils/helpers";
+import { TextArea, Form, Row, Container, Button, Col } from "react-bootstrap";
 
 function Contact() {
-    //assigning state variables, and setting inital states
-    const [name, setName] = useState('');
-    const [email, setEmail]= useState('');
-    const [message, setMessage] = useState('');
+  //assigning state variables, and setting inital states
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    const handleInputChange = (e) => {
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-        if (inputType === 'email') {
-            setEmail(inputValue);
-    } else if (inputType ==='name') {
-            setName(inputValue);
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "name") {
+      setName(inputValue);
     } else {
-        setMessage(inputValue);
-    };
-};
+      setMessage(inputValue);
+    }
+  };
 
-    const handleFormSubmit = (e) => {
-        //prevents page reload upon submission
-        e.preventDefault();
+  const handleFormSubmit = (e) => {
+    //prevents page reload upon submission
+    e.preventDefault();
 
-        if (!validateEmail(email) || !name || !message) {
-            alert(`Email Invalid and no name or message submitted!`)
-            setEmail('');
-            setName('');
-            setMessage('');
-            return;
-        }
-
-        alert(`Thanks for your question ${name}!`)
-
-        setEmail('');
-        setName('');
-        setMessage('');
+    if (!validateEmail(email) || !name || !message) {
+      alert(`Email Invalid and no name or message submitted!`);
+      setEmail("");
+      setName("");
+      setMessage("");
+      return;
     }
 
-    return(
-        //form fields
-        <div>
-        <p>If you have a question please fill out the form below!</p>
-        <form className="form">
-            <input
-              value={name}
-              name="name"
-              onChange={handleInputChange}
+    alert(`Thanks for your question ${name}!`);
+
+    setEmail("");
+    setName("");
+    setMessage("");
+  };
+
+  return (
+    //form fields
+    // <Container>
+    //   <h2>If you have a question please fill out the form below!</h2>
+    //     <Row>
+    //       <input
+    //         value={name}
+    //         style={{ width: "30vw", background: "white", color: "black",}}
+    //         name="name"
+    //         onChange={handleInputChange}
+    //         type="text"
+    //         placeholder="name"
+    //       />
+    //     </Row>
+    //     <br></br>
+    //     <Row>
+    //       <input
+    //         value={email}
+    //         style={{ width: "30vw", background: "white", color: "black" }}
+    //         name="email"
+    //         onChange={handleInputChange}
+    //         type="email"
+    //         placeholder="email"
+    //       />
+    //     </Row>
+    //     <br></br>
+    //     <Row>
+    //     <textarea
+    //       value={message}
+    //       style={{
+    //         height: "30vh",
+    //         width: "30vw",
+    //         background: "white",
+    //         color: "black",
+    //       }}
+    //       name="message"
+    //       onChange={handleInputChange}
+    //       type="text"
+    //       placeholder="message"
+    //     />
+    //     </Row>
+    //     <Row>
+    //        <Row>
+    //       <Button variant="outline-light" style={{ width: '5vw' }} onClick={handleFormSubmit}>
+    //         Submit
+    //       </Button>
+    //     </Row>
+    //     </Row>
+    // </Container>
+    <Container>
+      <Form>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="name">
+            <Form.Label>Name:</Form.Label>
+            <Form.Control
               type="text"
-              placeholder="name"
+              name="name"
+              placeholder="Enter Your Name"
+              onChange={handleInputChange}
             />
-          <input
-            value={email}
-            name="email"
-            onChange={handleInputChange}
-            type="email"
-            placeholder="email"
-          />
-          <input
-            value={message}
-            name="message"
-            onChange={handleInputChange}
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="email">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter Your Email"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+        </Row>
+        <Form.Group as={Col} controlId="message">
+          <Form.Label>Message:</Form.Label>
+          <Form.Control
+            as="textarea"
             type="text"
-            placeholder="message"
+            style={{ height: "25vh" }}
+            name="message"
+            placeholder="Enter Your Message"
+            onChange={handleInputChange}
           />
-          <button type="button" onClick={handleFormSubmit}>Submit</button>
-        </form>
-      </div>
-    );
-    
+        </Form.Group>
+
+        <Button
+          variant="outline-light"
+          style={{ width: "5vw", justifyContent: "end" }}
+          onClick={handleFormSubmit}
+        >
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
 export default Contact;
